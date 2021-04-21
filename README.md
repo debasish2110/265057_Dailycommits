e45e2da645396c34dae07a47193db0cde144e4b0
debasish2110 <debasishdash98@gmail.com>
2021-04-21 18:03:27 +0530

    string functions(20/04)

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ P.s: it wasn't asked to upload the certificates here but i am keeping the certif
 | 4 | codes | [Book_Title_Sololearn_soln.py](https://github.com/debasish2110/265057_Dailycommits/blob/master/codes/Book_Title_Sololearn_soln.py) | 19/04 |
 | 5 | certs | [sololearn python certificate.pdf](https://github.com/debasish2110/265057_Dailycommits/blob/master/certs/sololearn%20python%20certificate.pdf) | 19/04 |
 | 6 | codes | [lists_in_python.py](https://github.com/debasish2110/265057_Dailycommits/blob/master/codes/lists_in_python.py) | 20/04 |
-| 7 | codes | [python_strings.py]() | 21/04 |
+| 7 | codes | [python_strings.py](https://github.com/debasish2110/265057_Dailycommits/blob/master/codes/python_strings.py) | 21/04 |
 **If you like my work please leave a :star:**
 
 ---------------------------------------------
--- codes/python_strings.py
+++ codes/python_strings.py
@@ -1,6 +1,6 @@
 
 string1="Welcome to LTTS"
-print("the input string is:",string1)
+print("the input string1 is:",string1)
 
 string2="""this 
 way 
@@ -8,3 +8,21 @@ we
 can 
 write a multiline string"""
 print(string2)
+
+#Accessing character in the string
+print("first character in the string1 is: ", string1[0])
+print("last character of the string1 is: ", string1[-1])
+
+#string slicing
+print("3rd to 9th of string1: ", string1[2:10])
+print("3rd and 2nd last char: ", string1[2:-2])
+print("reversing the string using slicing: ", string1[::-1])
+"""string1[2]='E'
+print("upading the 2nd char the new string is: (we cannot update a character in the string it will throw an error) ", string1)
+"""
+string1="I AM LTTS NEXT GEN"
+print("after upating the whole string: ", string1)
+
+"""same way we can not delete a character from a string but we can delete an entire string"""
+
+
