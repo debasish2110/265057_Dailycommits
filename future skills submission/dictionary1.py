ac60c9c401b813870e812950f67e8c87e7c588b7
debasish2110 <debasishdash98@gmail.com>
2021-04-22 23:31:13 +0530

    futureskill submissions(22/04)

---
+++ future skills submission/dictionary1.py
@@ -0,0 +1,17 @@
+""""
+Write a Python script to merge two Python dictionaries.
+"""
+def dict_merge(dict1,dict2):
+    merge=dict1 | dict2
+    return merge
+
+dict1={"Name":"Deba", "Age":22}
+dict2={"height":5.8, "weight":"68kg"}
+dm= dict_merge(dict1,dict2)   # storing the output of the dict_merge in the variable dm
+print(dm)
+
+#this code is contributed by Debashish Dash
+
+"""Write a Python program to convert a list into a nested dictionary of keys"""
+
+
