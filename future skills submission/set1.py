9a9aa8bb52a7973d98603a63327bce0355b722a8
debasish2110 <debasishdash98@gmail.com>
2021-04-23 17:31:51 +0530

    set qstn 1 soln submission(23/04)

---
+++ future skills submission/set1.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to find maximum and the minimum value in a set
+"""
+
+my_set = set([5, 10, 3, 15, 2, 20,3])
+
+print("Maximum value of the set is: ",max(my_set))
+
+print("minimum value of the set is: ",min(my_set))
